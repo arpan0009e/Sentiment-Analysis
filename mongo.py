673d608c59e20265677d227d49a6b2cbb1252365
Arpan Mondal <arpanmondl708@gmail.com>
2026-05-10 17:18:46 +0530

    Remove mongo.py from tracking

--- mongo.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-uri = "mongodb+srv://arpanmondal708_db_user:Arpan123@sentiment-cluster.ku7ncam.mongodb.net/?appName=sentiment-cluster"
-
-client = MongoClient(uri)
-
-print(client.list_database_names())
